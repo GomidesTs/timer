8405ab39e697ef5b4dce69d35bb88326f2cba0e5
GomidesTs <gomidestulio5@gmail.com>
2022-08-21 18:42:19 -0300

    feat: capturing the button elements responsible for the application's application dynamism

---
+++ src/js/elements.js
@@ -0,0 +1,19 @@
+const buttonPlay = document.getElementById('play')
+const buttonPause = document.getElementById('pause')
+const buttonStop = document.getElementById('stop')
+const buttonSet = document.getElementById('set')
+const buttonSoundOn = document.getElementById('sound-on')
+const buttonSoundOff = document.getElementById('sound-off')
+const minutesDisplay = document.getElementById('minutes')
+const secondsDisplay = document.getElementById('seconds')
+
+export {
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSet,
+    buttonSoundOn,
+    buttonSoundOff,
+    minutesDisplay,
+    secondsDisplay
+}
